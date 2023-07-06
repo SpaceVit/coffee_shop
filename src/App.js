f5f4f4a68564f52e90e61cf5df41b5a752ba8446
Dmitry Kolodchenko <92672116+DKolod@users.noreply.github.com>
2023-07-06 22:50:18 +0300

    add img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import main from 'images/main.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={main} className="Main-logo" alt="main-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
