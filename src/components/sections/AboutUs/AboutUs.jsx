9aeb098e063a7ea888564dbe1247bd13299e1f4a
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2023-07-08 18:05:33 +0300

    +

--- src/components/sections/AboutUs/AboutUs.jsx
+++ src/components/sections/AboutUs/AboutUs.jsx
@@ -4,7 +4,8 @@ import styles from './AboutUs.module.scss';
 const AboutUs = () => {
   return (
     <div className={styles.aboutUsContainer}>
-      <p>ABOUT US</p>
+      <h2>ABOUT US</h2>
+      <p>Lorem ipsum dolor sit amet consectetur. Adipiscing sodales neque vestibulum tincidunt nibh. In nibh quis quis pellentesque. Amet a maecenas varius sit volutpat quam euismod vulputate aliquam. Semper leo morbi ut mattis orci auctor vitae.</p>
     </div>
   );
 };
