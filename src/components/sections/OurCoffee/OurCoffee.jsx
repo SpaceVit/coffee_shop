57e67d1005ac3102aef178fd79c1ce6c38378a11
Vitaliy Pavlovskiy <sledozar@gmail.com>
2023-07-08 09:42:30 +0300

    addFonts

--- src/components/sections/OurCoffee/OurCoffee.jsx
+++ src/components/sections/OurCoffee/OurCoffee.jsx
@@ -4,7 +4,7 @@ import styles from './OurCoffee.module.scss';
 const OurCoffee = () => {
   return (
     <div className={styles.ourCoffeeContainer}>
-      <h1>OUR COFFEE</h1>
+      <h1 className={styles.ourCoffeeTitle}>OUR COFFEE</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur. Adipiscing sodales neque vestibulum tincidunt nibh. In nibh quis quis pellentesque. Amet a maecenas varius sit
         volutpat quam euismod vulputate aliquam. Semper leo morbi ut mattis orci auctor vitae.
