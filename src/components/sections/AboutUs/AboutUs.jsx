36f85640dcf866a1eba7c455b802c76d00f490f4
Vitaliy Pavlovskiy <sledozar@gmail.com>
2023-07-10 08:45:32 +0300

    done our coffee

--- src/components/sections/AboutUs/AboutUs.jsx
+++ src/components/sections/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
     <div id="About" className={styles.aboutUsContainer}>
       <h2>ABOUT US</h2>
       <p>Lorem ipsum dolor sit amet consectetur. Adipiscing sodales neque vestibulum tincidunt nibh. In nibh quis quis pellentesque. Amet a maecenas varius sit volutpat quam euismod vulputate aliquam. Semper leo morbi ut mattis orci auctor vitae.</p>
-        <SocialLinkList id="Contacts"/>
+      <SocialLinkList id="Contacts"/>
     </div>
   );
 };
